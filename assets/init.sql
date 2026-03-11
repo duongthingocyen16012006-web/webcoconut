6a13fdabf5d02f3357a69798d0e1c0a8bed49404
ngocyen <ngocyen1601@gmai.com>
2026-03-11 12:17:02 +0000

    webcoconut50%

---
+++ assets/init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE KeoDua (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ten_keo VARCHAR(100),
+    huong_vi VARCHAR(50),
+    gia_tien INT
+);
+INSERT INTO KeoDua (ten_keo, huong_vi, gia_tien) VALUES 
+('Kẹo dừa truyền thống', 'Nguyên bản', 30000),
+('Kẹo dừa lá dứa', 'Lá dứa', 35000),
+('Kẹo dừa đậu phộng', 'Đậu phộng', 40000),
+('Kẹo dừa sầu riêng', 'Sầu riêng', 45000);
